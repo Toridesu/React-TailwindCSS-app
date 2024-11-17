58932330f58e79f871be354ecf8d248105c28596
Toridesu <160250540+Toridesu@users.noreply.github.com>
2024-11-17 20:26:42 +0900

    TailWindCSSの練習、ルーティングの作成

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Router } from './router/Router';
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <button className='bg-indigo-700 font-semibold text-white py-2 px-4 rounded'>ボタン</button>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="flex min-h-screen items-center justify-center bg-gray-100 text-gray-800">
+          <Router />
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
+// <h1 className="text-3xl font-bold underline">Hello world!</h1>
+// <button className="rounded bg-indigo-700 px-4 py-2 font-semibold text-white">
+//   ボタン
+// </button>
 export default App;
---
+++ my-app/src/components/pages/Page404.tsx
@@ -0,0 +1,5 @@
+import { FC, memo } from "react";
+
+export const Page404: FC = memo(() => {
+  return <p>このページは存在しないか削除された可能性があります</p>;
+});
